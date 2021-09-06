14e702e42d31cef955d5f296dc4ca3f82400b48d
aleksanderkoder <neon.113@hotmail.com>
2021-09-06 02:08:38 +0200

    Queue functionality (needs testing)

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 
 const client = new Discord.Client();
-var connection;
-var vc;
+let connection;
+let vc;
+let songQueue = []; 
+let playing = false; 
+let counter = 0;
 
 client.on("ready", () => {
   console.log("Bot is ready");
@@ -13,23 +16,38 @@ client.on("ready", () => {
 
 client.login(process.env.BOT_TOKEN);
 
-client.on("message", async (message) => {
+client.on("message", (message) => {
   // if (msg.content === '!play') {
   //     const channel = client.channels.get("150685223540097024");
   //     channel.join();
   //     console.log("Joined voice channel!");
   // }
+  playSong(message); 
+});
+
+async function playSong(message) {
   if (
     message.content.includes(
       "!play https://furymusicplayer.000webhostapp.com/"
     ) &&
     message.member.voice.channel
-  ) {
+  ) { 
+   
+    if(playing) {
+       songQueue.push(message);
+      return;
+    }; 
     let msgParams = message.content.split(" ");
     let url = new URL(msgParams[1]);
     let songid = url.searchParams.get("songid");
+    playing = true; 
+    
+    console.log(JSON.stringify(songQueue)); 
+    
     vc = message.member.voice.channel;
     connection = await message.member.voice.channel.join();
+    
+
     // Create a dispatcher
     //const dispatcher = connection.play('https://furymusicplayer.000webhostapp.com/songs/LXST CXNTURY - INFINITY VOLUME TWO.mp3');
 
@@ -41,7 +59,7 @@ client.on("message", async (message) => {
       .then((text) => {
         console.log(text);
         if (text != "Error") {
-          const dispatcher = connection.play(text);
+          const dispatcher = connection.play(text); 
 
           dispatcher.on("start", () => {
             console.log("Audio is now playing!");
@@ -49,11 +67,18 @@ client.on("message", async (message) => {
 
           dispatcher.on("finish", () => {
             console.log("Audio has finished playing!");
-            
-            setTimeout(() => {
-              vc.leave();
-            }, 5000);
-            
+            playing = false;
+            console.log(songQueue.length); 
+
+            if(songQueue.length > 1) {
+              songQueue.shift();
+              playSong(songQueue[0]);
+            } else if( songQueue.length == 1) {
+              playSong(songQueue[0]);
+              songQueue.shift();
+            } else {
+              vc.leave(); 
+            }
           });
 
           // Always remember to handle errors appropriately!
@@ -69,6 +94,7 @@ client.on("message", async (message) => {
     // connection.play('http://myserver.com/audio.aac');
   } else if (message.content == "!stop") {
     vc.leave();
+    songQueue = []; 
     console.log("Left voice channel");
   }
-});
+}
